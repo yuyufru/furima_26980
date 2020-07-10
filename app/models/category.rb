f019675a5a80f2a2f8c32022ab05a793dccdfc2f
yuyufru <y3ff2638.aad223@gmail.com>
2020-07-11 08:27:58 +0900

    11th1
    11th1

---
+++ app/models/category.rb
@@ -0,0 +1,9 @@
+class  Category< ActiveHash::Base
+
+  self.data = [
+    {id: 1, name: 'レディース'}, {id: 2, name: 'ベビー・キッズ'}, {id: 3, name: '本・音楽・ゲーム'},
+    {id: 4, name: 'おもちゃ・ホビー・グッズ'}, {id: 5, name: '家電・スマホ・カメラ'}, {id: 6, name: 'スポーツ・レジャーハンドメイド'},
+    {id: 7, name: 'その他'}, 
+  ]
+end
+
