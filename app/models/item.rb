b725498d45860503e689675019c87bfd314ec2f2
yuyufru <y3ff2638.aad223@gmail.com>
2020-07-12 11:54:46 +0900

    12th2
    12th2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,10 @@
 class Item < ApplicationRecord
+  
   has_one_attached :image
-
-#   #空の投稿を保存できないようにする
-#   validates :category, :status, :postage, :shpping_date, :prefecture, presence: true
-
-#   #選択が「--」のままになっていないか
-#   with_options numericality: { other_than: 1 } do
-#   validates :item_id
-#   end
-
-  # belongs_to :address
-  # has_one :item_purchase
+  belongs_to :user
+  belongs_to :address
+  has_one :item_purchase
+ 
   # belongs_to_active_hash :item_id
 end  
 
